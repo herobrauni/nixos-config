89a7c66d578669b690b5bf54a4d7394378b56ea4
herobrauni <braunnicolaj@gmail.com>
2024-02-13 17:58:58 +0100

    renamed:    hosts/envy/flake.nix -> hosts/envy/default.nix 	renamed:    patches/sound_envy/1.patch -> hosts/envy/sound_envy/1.patch 	renamed:    patches/sound_envy/2.patch -> hosts/envy/sound_envy/2.patch 	renamed:    patches/sound_envy/3.patch -> hosts/envy/sound_envy/3.patch 	renamed:    patches/sound_envy/4.patch -> hosts/envy/sound_envy/4.patch 	renamed:    patches/sound_envy/5.patch -> hosts/envy/sound_envy/5.patch

--- hosts/envy/default.nix
+++ hosts/envy/default.nix
@@ -18,23 +18,23 @@
   boot.kernelPatches = [
     {
       name = "sound_1";
-      patch = /etc/nixos/patches/sound_envy/1.patch;
+      patch = ./sound_envy/1.patch;
     }
     {
       name = "sound_2";
-      patch = /etc/nixos/patches/sound_envy/2.patch;
+      patch = ./sound_envy/2.patch;
     }
     {
       name = "sound_3";
-      patch = /etc/nixos/patches/sound_envy/3.patch;
+      patch = ./sound_envy/3.patch;
     }
     {
       name = "sound_4";
-      patch = /etc/nixos/patches/sound_envy/4.patch;
+      patch = ./sound_envy/4.patch;
     }
     {
       name = "sound_5";
-      patch = /etc/nixos/patches/sound_envy/5.patch;
+      patch = ./sound_envy/5.patch;
     }
   ];
 
