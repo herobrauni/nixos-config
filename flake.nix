aefdd7d6a43388fbc537ed8894b8def1487f9c15
herobrauni <braunnicolaj@gmail.com>
2024-02-15 12:36:39 +0100

    modified:   flake.nix 	modified:   hosts/envy/home.nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+      nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     impermanence.url = "github:nix-community/impermanence";
@@ -29,7 +30,7 @@
             home-manager.useUserPackages = true;
 
             home-manager.extraSpecialArgs = inputs;
-#            home-manager.users.brauni = import ./home;
+            home-manager.users.brauni = import ./hosts/envy/home.nix;
           }
         ];
       };
