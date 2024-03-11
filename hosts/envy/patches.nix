887fd3794a3d8cba2f5c82fb57882ba3ad212d47
brauni <braunnicolaj@gmail.com>
2024-03-11 12:54:27 +0100

    removed patches for now

--- hosts/envy/patches.nix
+++ hosts/envy/patches.nix
@@ -3,26 +3,26 @@
 {
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  boot.kernelPatches = [
-    {
-      name = "sound_1";
-      patch = ./sound_envy/1.patch;
-    }
-    {
-      name = "sound_2";
-      patch = ./sound_envy/2.patch;
-    }
-    {
-      name = "sound_3";
-      patch = ./sound_envy/3.patch;
-    }
-    {
-      name = "sound_4";
-      patch = ./sound_envy/4.patch;
-    }
-    {
-      name = "sound_5";
-      patch = ./sound_envy/5.patch;
-    }
-  ];
+#  boot.kernelPatches = [
+#    {
+#      name = "sound_1";
+#      patch = ./sound_envy/1.patch;
+#    }
+#    {
+#      name = "sound_2";
+#      patch = ./sound_envy/2.patch;
+#    }
+#    {
+#      name = "sound_3";
+#      patch = ./sound_envy/3.patch;
+#    }
+#    {
+#      name = "sound_4";
+#      patch = ./sound_envy/4.patch;
+#    }
+#    {
+#      name = "sound_5";
+#      patch = ./sound_envy/5.patch;
+#    }
+#  ];
 }
