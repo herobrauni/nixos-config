2bbc808a61f8e3f429e4e4e85c457ae3b17d3494
brauni <braunnicolaj@gmail.com>
2024-03-22 15:03:24 +0100

    bla

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "brauni NixOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+#    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+#    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # home-manager.url = "github:nix-community/home-manager/release-23.11";
     # home-manager.inputs.nixpkgs.follows = "nixpkgs";
     impermanence.url = "github:nix-community/impermanence";
