1250e03e8b2812684496e7c20ead0856bf8ad5dd
herobrauni <braunnicolaj@gmail.com>
2024-07-17 20:05:32 +0200

    asd

--- hosts/puck/configuration.nix
+++ hosts/puck/configuration.nix
@@ -19,6 +19,12 @@
     };
   };
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/etc/nixos";
+  };
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
