a85a628cfc8993d8565ec89d015589a65032bb12
herobrauni <braunnicolaj@gmail.com>
2024-07-17 21:04:38 +0200

    new file:   hosts/puck/ephemeral.nix 	new file:   hosts/puck/persistence.nix 	new file:   mist.nix 	new file:   modules/nixos/ephemeral.nix

---
+++ hosts/puck/persistence.nix
@@ -0,0 +1,48 @@
+{
+  # https://github.com/nix-community/impermanence/blob/master/README.org#nixos
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = [
+      "/etc/ssh"
+      "/var/lib/systemd/timers"
+      "/etc/nixos" # not everyone seemed to do this - maybe i just need this for the age key?
+      "/var/db/sudo"
+      "/var/lib/bluetooth"
+      "/var/lib/nixos" # important nixos files like uid/gid map
+      "/var/lib/systemd"
+      "/var/lib/systemd/coredump"
+      "/etc/NetworkManager/system-connections"
+    ];
+    files = [
+      "/etc/adjtime" # hardware clock offset
+    ];
+    users.brauni = {
+      directories = [
+        "Downloads"
+        "Music"
+        "Pictures"
+        "Documents"
+        "Videos"
+        "Nextcloud"
+        {
+          directory = ".gnupg";
+          mode = "0700";
+        }
+        {
+          directory = ".ssh";
+          mode = "0700";
+        }
+        {
+          directory = ".local/share/keyrings";
+          mode = "0700";
+        }
+        ".local/share/nix" # trusted settings and repl history
+        ".local/share/mime"
+        ".local/share/icons"
+        ".wine"
+        ".cache/nix"
+        ".config/sops/age"
+      ];
+    };
+  };
+}
