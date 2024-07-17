e39c776ef2f06cf641a431ede12487e610a2ab00
herobrauni <braunnicolaj@gmail.com>
2024-07-17 20:02:30 +0200

    switch config to mysterio template

--- flake.nix
+++ flake.nix
@@ -21,59 +21,59 @@
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
     nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
 
-  outputs =
-    inputs @ { self
-    , nixpkgs
-    , home-manager
-    , agenix
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    agenix
     , impermanence
     , lanzaboote
-    , NixOS-WSL
-    , flake-parts
-    , ...
-    }: {
-      nixosConfigurations = {
-        envy = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            impermanence.nixosModules.impermanence
-            lanzaboote.nixosModules.lanzaboote
-            ./hosts/envy
-          ];
-        };
-        deskbrauni = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            impermanence.nixosModules.impermanence
-            ./hosts/deskbrauni
-          ];
-        };
-        ocismall1 = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/ocismall1
-          ];
-        };
-        wsl = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/wsl
-            NixOS-WSL.nixosModules.wsl
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              # home-manager.users.brauni = import ./home;
-            }
-          ];
-        };
+
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    # Supported systems for your flake packages, shell, etc.
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    # This is a function that generates an attribute by calling a function you
+    # pass to it, with each system as an argument
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    # Your custom packages
+    # Accessible through 'nix build', 'nix shell', etc
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    # Formatter for your nix files, available through 'nix fmt'
+    # Other options beside 'alejandra' include 'nixpkgs-fmt'
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+    # Your custom packages and modifications, exported as overlays
+    overlays = import ./overlays {inherit inputs;};
+    # Reusable nixos modules you might want to export
+    # These are usually stuff you would upstream into nixpkgs
+    nixosModules = import ./modules/nixos;
+    # Reusable home-manager modules you might want to export
+    # These are usually stuff you would upstream into home-manager
+    homeManagerModules = import ./modules/home-manager;
+
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      # FIXME replace with your hostname
+      puck= nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main nixos configuration file <
+          ./hosts/puck/configuration.nix
+        ];
       };
     };
+  };
 }
