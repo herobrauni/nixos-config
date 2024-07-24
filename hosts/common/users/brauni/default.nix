8cd8e49edb2447fe719c1352c03684459265f2ed
brauni <braunnicolaj@gmail.com>
2024-07-24 23:31:43 +0200

    ssh.pub

--- hosts/common/users/brauni/default.nix
+++ hosts/common/users/brauni/default.nix
@@ -7,30 +7,30 @@
   ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
 in {
   users.mutableUsers = false;
-  # users.users.brauni = {
-  #   isNormalUser = true;
-  #   shell = pkgs.fish;
-  #   extraGroups = ifTheyExist [
-  #     "audio"
-  #     "deluge"
-  #     "docker"
-  #     "git"
-  #     "i2c"
-  #     "libvirtd"
-  #     "lxd"
-  #     "minecraft"
-  #     "mysql"
-  #     "network"
-  #     "podman"
-  #     "video"
-  #     "wheel"
-  #     "wireshark"
-  #   ];
+  users.users.brauni = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+    extraGroups = ifTheyExist [
+      "audio"
+      "deluge"
+      "docker"
+      "git"
+      "i2c"
+      "libvirtd"
+      "lxd"
+      "minecraft"
+      "mysql"
+      "network"
+      "podman"
+      "video"
+      "wheel"
+      "wireshark"
+    ];
 
-  #   openssh.authorizedKeys.keys = lib.splitString "\n" (builtins.readFile ../../../../home/brauni/ssh.pub);
+    openssh.authorizedKeys.keys = lib.splitString "\n" (builtins.readFile ../../../../home/brauni/ssh.pub);
     hashedPasswordFile = config.sops.secrets.brauni-password.path;
     packages = [pkgs.home-manager];
-  # };
+  };
 
   sops.secrets.brauni-password = {
     sopsFile = ../../secrets.yaml;
