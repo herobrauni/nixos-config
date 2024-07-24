f406473db42d3145d0dc3232215d47e3750c1ed1
brauni <braunnicolaj@gmail.com>
2024-07-24 21:26:35 +0200

    bara

--- hosts/bara/default.nix
+++ hosts/bara/default.nix
@@ -13,7 +13,7 @@
       ./hardware-configuration.nix
       ./disko-config.nix
       ../common/global
-      ../common/optional/encrypted-root.nix
+      # ../common/optional/encrypted-root.nix
       ../common/optional/ephemeral-btrfs.nix
     ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -30,28 +30,6 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.consoleLogLevel = 0;
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = true;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = true;
-
-    # Enable the Nvidia settings menu,
-	  # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-  };
-
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.graphics.enable = true;
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
--- hosts/bara/hardware-configuration.nix
+++ hosts/bara/hardware-configuration.nix
@@ -20,11 +20,12 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
-    powerManagement.enable = false;
+    powerManagement.enable = true;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+    # powerManagement.finegrained = true;
+    powerManagement.finegrained = lib.mkForce false;
 
     # Enable the Nvidia settings menu,
 	  # accessible via `nvidia-settings`.
